4a22ce72dd74faf6cf33f1ed07bf433f019a676f
bfgonzalez <bfgonzalez142@gmail.com>
2018-08-06 14:09:11 +0800

    chore: remove unnecessary spaces

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 var api = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-var getQuote = function(data) {
 
+var getQuote = function(data) {
   if (data.quoteAuthor === "") {
     	data.quoteAuthor = "Unknown";
   }
-
   $(".quote-text").text(data.quoteText);
   $(".quote-author").text("- " + data.quoteAuthor);
-
   $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + data.quoteText + '"' + ' - ' +  data.quoteAuthor));
 };
 
@@ -18,7 +16,5 @@ var firstAuthor = "Aristotle";
 $(".tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + firstQuote + '"' + ' - ' + firstAuthor));
 
 $(".get-quote").on("click", function() {
-
 	$.getJSON(api, getQuote, "jsonp");
-
 });
